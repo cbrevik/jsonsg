c61035d30d1b0b019c761e4434c6d6a23e83aa3b
Christian Brevik <christian.brevik@gmail.com>
2020-07-12 13:23:22 +0200

    init

---
+++ Generator/JsonSourceGenerator.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace Generator1
+{
+    [Generator]
+    public class JsonSourceGenerator : ISourceGenerator
+    {
+        public void Execute(SourceGeneratorContext context)
+        {
+            // Using the context, get any additional files that end in .xmlsettings
+            IEnumerable<AdditionalText> settingsFiles = context.AdditionalFiles.Where(at => at.Path.EndsWith(".json"));
+            foreach (var file in settingsFiles)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file.Path);
+                var source = GetClass(
+                    fileName,
+                    JsonDocument.Parse(file.GetText().ToString()).RootElement
+                );
+                context.AddSource($"Json_{fileName}.cs", SourceText.From(source, Encoding.UTF8));
+            }
+        }
+
+        static string GetClass(string name, JsonElement el)
+        {
+            var sb = new StringBuilder($@"
+    public class {name} 
+    {{");
+            foreach (var prop in el.EnumerateObject())
+            {
+                sb.AppendLine($"public {GetPropertyType(prop.Value.ValueKind)} {prop.Name} {{ get; set; }}");
+            }
+
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        static string GetPropertyType(JsonValueKind kind) =>
+            kind switch
+            {
+                JsonValueKind.String => "string",
+                _ => "int"
+            };
+
+        public void Initialize(InitializationContext context)
+        {
+        }
+    }
+}
---
+++ jsonsource/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace jsonsource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonString = File.ReadAllText("./Spotify/me.json");
+            var jsonDocument = JsonDocument.Parse(jsonString);
+
+            PrintElement(jsonDocument.RootElement);
+        }
+
+        static void PrintElement(JsonElement el)
+        {
+            switch (el.ValueKind)
+            {
+                case JsonValueKind.Object:
+                    Console.WriteLine($"Object:");
+                    foreach (var prop in el.EnumerateObject())
+                    {
+                        PrintProperty(prop);
+                    }
+                    break;
+                case JsonValueKind.String:
+                    Console.WriteLine($"String value: {el.GetString()}");
+                    break;
+                default:
+                    Console.WriteLine("No idea");
+                    break;
+            }
+        }
+
+        static void PrintProperty(JsonProperty prop)
+        {
+            Console.WriteLine($"Prop name: {prop.Name}");
+            PrintElement(prop.Value);
+        }
+    }
+}
